97f7fe73b7541cec0643357fa9ac8eeaa413e30e
Akgithhub <ak08118967@gmail.com>
2024-08-04 08:59:45 -0700

    config files

--- server/server.js
+++ server/server.js
@@ -1,2 +1,14 @@
-import express from 'express'
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
+app.use(cors());
+
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Hello Farmers");
+});
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server is running on port 3000");
+});
