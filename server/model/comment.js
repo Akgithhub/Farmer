a939fd034530abefc21eb23fa02374da728bc307
Akgithhub <ak08118967@gmail.com>
2024-08-10 05:57:32 -0700

    backend comment controlls

--- server/model/comment.js
+++ server/model/comment.js
@@ -2,7 +2,6 @@ import mongoose from "mongoose";
 const commentSchema = new mongoose.Schema(
   {
     userId: { type: String, required: true },
-    videoId: { type: String, required: true },
     desc: { type: String, required: true },
   },
   { timestamps: true }
