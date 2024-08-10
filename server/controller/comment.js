a0e61ba48e196da01f4dc95dbc19750dbdd075a8
Akgithhub <ak08118967@gmail.com>
2024-08-10 06:19:01 -0700

    updated comment controllers

--- server/controller/comment.js
+++ server/controller/comment.js
@@ -2,7 +2,7 @@ import Comment from "../model/comment.js";
 
 export const addComment = async (req, res) => {
   const comment = req.body;
-  const newComment = new Comment(comment);
+  const newComment = new Comment(comment, req.user._id);
   try {
     await newComment.save();
     res.status(200).json({
@@ -18,5 +18,76 @@ export const addComment = async (req, res) => {
   }
 };
 // delete comment
+export const deleteComment = async (req, res) => {
+  const commentId = req.params.id;
+  try {
+    const deleteCommentById = await Comment.findByIdAndDelete(commentId);
+    if (deleteCommentById) {
+      res.json({
+        success: true,
+        message: "comment deleted successfully",
+        data: deleteCommentById,
+      });
+    } else {
+      res.json({
+        success: false,
+        message: "comment not found",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: "error while deleteing comment",
+      error: error.message,
+    });
+  }
+};
 // update comment
+export const updateComment = async (req, res) => {
+  try {
+    const commentId = req.params.id;
+    const updatedcomment = await Comment.findByIdAndUpdate(
+      commentId,
+      req.body,
+      { new: true }
+    );
+    if (updatedcomment) {
+      res.json({
+        success: true,
+        message: "comment updated successfully",
+        data: updatedcomment,
+      });
+    } else {
+      res.json({
+        success: false,
+        message: "comment not found",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: "error while updating comment",
+      error: error.message,
+    });
+  }
+};
 // getAll comment
+export const allComment = async (req, res) => {
+  try {
+    const Allcomments = await Comment.find();
+    res.json({
+      success: true,
+      message: "All comments",
+      data: Allcomments,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({
+      success: false,
+      message: "error while get all comment",
+      error: error.message,
+    });
+  }
+};
--- server/routes/comment.js
+++ server/routes/comment.js
@@ -1,14 +1,20 @@
 import express from "express";
 import ProtectRoute from "../middleware/ProtectRoute.js";
-import { addComment } from "../controller/comment.js";
+import {
+  addComment,
+  allComment,
+  deleteComment,
+  updateComment,
+} from "../controller/comment.js";
 
 const commentRouter = express.Router();
 
 // add comment
-commentRouter.post("/addComment",ProtectRoute, addComment);
+commentRouter.post("/addComment", ProtectRoute, addComment);
 // delete comment
-commentRouter.delete("/deleteComment");
+commentRouter.delete("/deleteComment/:id", ProtectRoute, deleteComment);
 // udate comment
-commentRouter.put("/updateComment");
+commentRouter.put("/updateComment/:id", ProtectRoute, updateComment);
+commentRouter.get("/allComment", allComment);
 
 export default commentRouter;
